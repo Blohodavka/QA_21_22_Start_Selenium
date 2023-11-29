1ae4df60ffe572e5f4458af41639c0323a22ecd2
Blohodavka <Blohodavka@mail.ru>
2023-11-29 21:00:15 +0200

    CssSelectors

---
+++ src/test/java/HomeWorks.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HomeWorks {
+WebDriver wd;
+
+@BeforeClass
+    public void setup(){
+
+}
+
+}
